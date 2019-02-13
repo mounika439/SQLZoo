bf9ab52dabdf003c2eb5b650c73ec86143c4f452
mouni439 <uppala.mounika13@gmail.com>
2019-02-13 14:27:29 -0500

    First Commit

---
+++ SELECT_within_SELECT.sql
@@ -0,0 +1,27 @@
+/* SELECT within SELECT Tutorial */
+
+
+1. /*List each country name where the population is larger than that of 'Russia'.
+
+	world(name, continent, area, population, gdp) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
